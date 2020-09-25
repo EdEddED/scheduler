72107350a4ade801334af54e661344247e06ef02
Enzo De Luca <enzo_deluca@outlook.com>
2020-09-25 12:06:06 -0400

    implemented more Appointment Elements

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,9 @@ import InterviewerList from "components/InterviewerList";
 
 import Appointment from "components/Appointment";
 import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
 
 storiesOf("Button", module)
   .addParameters({
@@ -140,4 +143,19 @@ storiesOf("Appointment", module)
   })
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
-  .add("Header", () => <Header time="12pm" />);
+  .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd = {action("onAdd")} />)
+  .add("Show", () => 
+    <Show 
+      student = "Lynda Miller-Jones" 
+      interviewer = {interviewer}
+      onEdit = {action("onEdit")}
+      onDelete = {action("onDelete")}
+      />)
+  .add("Confirm", () =>
+    <Confirm
+      message = "Delete the Appointment?"
+      onConfirm = {action("onConfirm")}
+      onCancel = {action("onCancel")}
+      />
+    )
