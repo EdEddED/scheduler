4fc5c5daf7c237c9a6547749d21ab81aef9987bf
Enzo De Luca <enzo_deluca@outlook.com>
2020-09-25 15:07:32 -0400

    implemened full appointment functionality

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 // import classNames from "classnames";
 import "components/Appointment/styles.scss"
 
+import Header from "components/Appointment/Header";
+import Show from "components/Appointment/Show";
+import Empty from "components/Appointment/Empty";
+
 export default function Appointment (props) {
   
+  const interview = props.interview;
+
   return(
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? (<Show 
+          student={interview.student} 
+          interviewer={interview.interviewer} 
+          onEdit={props.onEdit} 
+          onDelete={props.onDelete} 
+        />) : (<Empty />)}
+    </article>
   )
 
 }
